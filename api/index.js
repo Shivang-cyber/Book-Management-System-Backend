bf732e4d112c1aa7228e2352026dac13101f8c01
Shivang Shukla <iamshivang@outlook.com>
2024-11-07 09:05:04 +0530

    commit  testing vercel

--- api/index.js
+++ api/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const booksRouter = require('./routes/books');
-const userRouter = require('./routes/user');
-const errorHandler = require('./middleware/errorHandler');
+const booksRouter = require('../routes/books');
+const userRouter = require('../routes/user');
+const errorHandler = require('../middleware/errorHandler');
 const cors = require('cors');
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
 app.get('/', (req, res) => {
   res.send('Welcome to the Bookstore');
 });
@@ -16,7 +18,5 @@ app.use('/books', booksRouter);
 app.use('/auth', userRouter);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+module.exports = app;
